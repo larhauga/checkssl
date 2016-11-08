90062eae7dd920ec732ce9642c6a9d4199bcfefd
Lars Haugan <lars@larshaugan.net>
2016-11-08 23:19:54 +0100

    Threaded the operations. Use with care

--- checkssl.py
+++ checkssl.py
@@ -4,14 +4,46 @@
 import requests
 import json
 from time import sleep
-from multiprocessing import Process
+# from multiprocessing import Process
 import pprint
 import datetime
 
+from Queue import Queue
+from threading import Thread
+
 API_BASE='https://api.ssllabs.com/api/v2/'
 API_INFO='https://api.ssllabs.com/api/v2/info'
+OUTFILE='/tmp/output.json'
 
-OUTFILE='output.json'
+class Worker(Thread):
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try:
+                func(*args, **kargs)
+            except Exception, e:
+                print "In run exception"
+                print e
+            self.tasks.task_done()
+
+class ThreadPool:
+    def __init__(self, num_threads):
+        self.tasks= Queue(num_threads)
+        for _ in range(num_threads):
+            Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        self.tasks.put((func, args, kargs))
+
+    def wait_completion(self):
+        self.tasks.join()
+
 
 def api_available():
     r = requests.get(API_INFO)
@@ -27,11 +59,11 @@ def api_limit_reached():
         data = r.json()
         try:
             if int(data['maxAssessments']) > int(data['currentAssessments']):
-                print data['currentAssessments']
-                return True
-            else:
-                print data['currentAssessments']
+                print "Current assessments: %s" % str(data['currentAssessments'])
                 return False
+            else:
+                print "Current assessments in progress: %s" % str(data['currentAssessments'])
+                return True
         except:
             print "Parse data error: {}".format(str(data))
 
@@ -53,52 +85,75 @@ def analyze(url):
         data = r.json()
         return data
 
-def endpoint(endpoint):
-    endpoint['details']
-    'grade'
 
-
-def process_domain(domain):
-    test = analyze(domain)
-    if test['status'] == 'READY':
-        with open(OUTFILE, 'a') as f:
-            f.write("%s\n" %(json.dumps(test)))
-    while test['status'] != 'READY':
-        sleep(1)
+def process_domain(domain, n=None):
+    """Trying to process domain. do 5 checks if api busy"""
+    if not api_limit_reached() and n<5:
+        print "starting to analyze domain %s" % domain
         test = analyze(domain)
         if test['status'] == 'READY':
             with open(OUTFILE, 'a') as f:
-                f.write("%s\n" % (json.dumps(test)))
-                print "Done for %s" % (domain)
-        sleep(19)
+                f.write("%s\n" %(json.dumps(test)))
+        while test['status'] != 'READY':
+            print "Wating for %s" % domain
+            sleep(1)
+            test = analyze(domain)
+            if test['status'] == 'READY':
+                with open(OUTFILE, 'a') as f:
+                    f.write("%s\n" % (json.dumps(test)))
+                    print "Done for %s" % (domain)
+            sleep(19)
+    else:
+        if n:
+            n = n + 1
+        else:
+            n = 1
+        sleep(1)
+        process_domain(domain, n)
 
-def burst(domains):
-    threads = []
-    for domain in domains:
-        thread = Thread(target=process_domain, args=domain)
-        thread.start()
-        threads.append(thread)
-
-    for t in threads:
-        t.join()
-
-def gen_threads(domains):
-    threads = []
-    for domain in domains:
-        thread = Thread(target=process_domain, args=domain)
-        threads.append(thread)
-
-    return threads
+def find_domains():
+    ip_ranges= []
+    from netaddr import IPNetwork
+    import socket
+    domains = []
+    for ip in list(IPNetwork(ip_ranges[0])):
+        # print ip
+        try:
+            domains.append(socket.gethostbyaddr(str(ip))[0])
+        except socket.herror:
+            print "Unkown host %s" % ip
+        except socket.gaierror:
+            pass
+            #print "Name or service not known %s" % ip
+    # print domains
+    return domains
 
 def main():
-    check_list = []
-    if api_available():
-        for domain in check_list:
-            print "Testing domain %s" % domain
-            process_domain(domain)
-        print api_limit_reached()
-    else:
-        print "api not available"
+    #check_list =
+    check_list = find_domains()
+    # if api_available():
+        # for domain in check_list:
+            # print "Testing domain %s" % domain
+            # process_domain(domain)
+        # print api_limit_reached()
+    # else:
+        # print "api not available"
+
+    # delays = [randrange(1, 10) for i in range(1000)]
+
+    def wait_delay(d, i):
+        print 'This is one thread with delay in function %d' % i
+        print 'sleeping for (%d)sec' % d
+        sleep(d)
+
+    pool = ThreadPool(api_limit_free()-1)
+
+    for i, dom in enumerate(check_list):
+        print 'Batch start progress: %.2f%c' % ((float(i)/float(len(check_list)))*100.0,'%')
+
+        pool.add_task(process_domain, dom)
+        sleep(1)
+    pool.wait_completion()
 
 if __name__ == '__main__':
     main()
